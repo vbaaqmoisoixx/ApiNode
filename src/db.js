2b3a807aa8bc61df7db8473ce733cc2f8b593c00
contafdois-beep <contafdois@gmail.com>
2026-01-21 15:54:24 -0300

    first commit

--- src/db.js
+++ src/db.js
@@ -4,11 +4,12 @@ import dotenv from "dotenv";
 dotenv.config();
 const { Pool } = pkg;
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+export const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
 });
 
 export default pool;
